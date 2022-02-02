48400a33ccaf15d41930c96a461b4300d398ae58
Ivan Ocheretnyi <ocheret2413@gmail.com>
2022-02-03 01:20:51 +0200

    first js

---
+++ script/index.js
@@ -0,0 +1,26 @@
+/*let name = Ivan;
+let lastname = Ocheretnyi;
+let age = 30;
+
+
+// let 2name; let my-name; примеры не правильных имен переменных
+
+var city;
+const Year = 2000;
+
+/* let let; let %age; 
+еще один вариант не верных названий и способы коментирования в JS  
+*/
+
+
+let userName = prompt('Как тебя зовут?');
+let bornyear = prompt('В каком году ты родился?');
+const currentYear = 2022;
+let side = prompt('Чему равна сторона квадрата');
+let userAge = currentYear - bornyear;
+let perimetr = side * 4;
+
+
+console.log('Привет', userName)
+console.log('Тебе уже -', userAge)
+console.log('Периметр квадрата =', perimetr)
