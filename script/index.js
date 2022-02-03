83bd801e4452221c25f7a87d12d20998e1b0e5b9
Ivan Ocheretnyi <ocheret2413@gmail.com>
2022-02-03 23:54:49 +0200

    third_part

--- script/index.js
+++ script/index.js
@@ -24,3 +24,25 @@ let perimetr = side * 4;
 console.log('Привет', userName)
 console.log('Тебе уже -', userAge)
 console.log('Периметр квадрата =', perimetr)
+
+// Радиус окружности
+const Pnumber = 3.14;
+let radius = prompt('Какой радиус окружности?');
+let area = Pnumber * Math.pow(radius, 2);
+
+console.log('Площадь круга =', area);
+
+// Скорость 
+let distance = prompt('Каково растояние между городами?');
+let time = prompt('За сколько времени можно преодолеть это растояние?');
+let speed = distance / time;
+
+console.log('Тебе нужно двигаться со скоростью -', speed, 'км/ч');
+
+// Конвертор
+
+const course = 0.87;
+let dollar = prompt('Сколько доларов ты хочешь поменять?');
+let euro = dollar * course;
+
+console.log('У тебя получиться', euro, 'евро');
